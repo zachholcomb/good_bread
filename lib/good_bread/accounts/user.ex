9e7b5973dcd1582a0d3a5b5cf431be8d1d8a7774
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-27 14:20:16 -0600

    Adds user model

---
+++ lib/good_bread/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule GoodBread.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :address, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :address])
+    |> validate_required([:name, :email, :address])
+  end
+end
