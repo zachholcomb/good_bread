e585cef3d7eb8c42c8762962ab156693674d4c63
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-27 15:03:17 -0600

    Adds item model

---
+++ lib/good_bread/items/item.ex
@@ -0,0 +1,18 @@
+defmodule GoodBread.Items.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "items" do
+    field :name, :string
+    field :price, :float
+
+    timestamps()
+  end
+
+  @doc false
+  def changest(item, attrs) do
+    item
+    |> cast(attrs, [:name, :price])
+    |> validate_required([:name, :price])
+  end
+end
