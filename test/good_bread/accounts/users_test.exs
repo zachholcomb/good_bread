1ad5b0cc1bd81696cb41b2c3973fa75656cecfd3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-27 14:26:38 -0600

    Adds tests for invalid attrs and unique email address

--- test/good_bread/accounts/users_test.exs
+++ test/good_bread/accounts/users_test.exs
@@ -7,7 +7,7 @@ defmodule GoodBread.Accounts.UsersTest do
     alias GoodBread.Accounts.User
 
     @valid_attrs %{name: "Zach Holcomb", email: "byrdlike@gmail.com", address: "900 E. 8th Ave."}
-    @update_atts %{name: "Zerch Holcomb", email: "zach@example.com", address: "594 Hunter St."}
+    @update_attrs %{name: "Zerch Holcomb", email: "zach@example.com", address: "594 Hunter St."}
     @invalid_attrs %{name: nil, email: nil, address: nil}
 
     def user_fixture(attrs \\ %{}) do
@@ -24,6 +24,15 @@ defmodule GoodBread.Accounts.UsersTest do
       assert user.email == "byrdlike@gmail.com"
       assert user.name == "Zach Holcomb"
       assert user.address == "900 E. 8th Ave."
+    end
+
+    test "user cannot be create with invalid attributes" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+
+    test "user email is unique" do
+      user = user_fixture(@valid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@valid_attrs)
     end
   end
 end
